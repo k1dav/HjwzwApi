462b995d81ff8a73365c5a2c10c76b843b655175
Sung@Air <k1dave6412@ap-mic.com>
2020-02-12 12:37:25 +0000

    feat(models): books add time field

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,7 +1,7 @@
 from uuid import uuid4
-
+from datetime import datetime
 import sqlalchemy
-from sqlalchemy import Column, String, Text
+from sqlalchemy import Column, DateTime, String, Text
 from sqlalchemy.dialects.postgresql import UUID
 
 metadata = sqlalchemy.MetaData()
@@ -21,4 +21,13 @@ books = sqlalchemy.Table(
     Column("title", String(32), nullable=False),
     Column("author", String(32), nullable=False),
     Column("preface", Text),
+    
+    Column("created_at", DateTime, nullable=False, default=datetime.now),
+    Column(
+        "updated_at",
+        DateTime,
+        nullable=False,
+        default=datetime.now,
+        onupdate=datetime.now,
+    ),
 )
