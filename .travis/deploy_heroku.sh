5c989090928d8faf7d271c2045b21c9876365e0e
Sung@Air <k1dave6412@ap-mic.com>
2020-03-03 16:08:17 +0000

    build(cd): heroku config

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-heroku login
-heroku container:login
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push $PROJECT_NAME --app $HEROKU_APP_NAME
 heroku container:release $PROJECT_NAME --app $HEROKU_APP_NAME
